ba7c3773ade1134b438e1b93477e0b3f0ba498c8
boris riback <briback@ribackcereales.com>
2025-04-07 17:10:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,5 @@ def webhook():
         return f"Error al procesar el webhook: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
